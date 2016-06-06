35053c3040fb35a573eafbe90c414056a34f07af
hitomi <1670957065@qq.com>
2016-06-06 09:14:16 +0800

    test

--- app/src/main/java/com/hitomi/sortricheditor/MainActivity.java
+++ app/src/main/java/com/hitomi/sortricheditor/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnPosts;
 
+    private String str;
+
     private File mCurrentPhotoFile;// 照相机拍照得到的图片
 
     public static Intent getTakePickIntent(File f) {
